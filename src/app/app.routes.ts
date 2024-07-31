a83baa0dcba1250fc2e1a9f8a9c3d02a23bd7bf5
sanketpadgelwar <sanketpadgelwar39@gmail.com>
2024-07-31 18:15:14 +0530

    Dashboard with get all queries (count)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,14 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { CompassCalibrationOutlined } from '@mui/icons-material';
-import { HomepageComponent } from './homepage/homepage.component';
+// import { CompassCalibrationOutlined } from '@mui/icons-material';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectComponent } from './project/project.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-export const routes: Routes = [
-  { path: 'projects', component: ProjectComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  // { path: 'projects', component: ProjectComponent },
-  //   { path: 'activity', component: ActivityComponent },
-  //   { path: 'messages', component: MessagesComponent },
-  //   { path: 'members', component: MembersComponent },
-  //   { path: 'calendar', component: CalendarComponent },
-  //   { path: 'settings', component: SettingsComponent },
-  //   { path: 'new-update', component: NewUpdateComponent },
-  // Wildcard route for a 404 page or redirect
 
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: 'users', component: UserComponent },
-  // { path: 'projects', component: ProjectComponent },
-  // { path: 'tasks', component: TaskComponent },
-  // { path: 'home', component: HomepageComponent },
-  // { path: 'nav', component: NavbarComponent },
-  // { path: 'profile', component: HomepageComponent },
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'projects', component: ProjectComponent },
+
+  { path: 'nav', component: NavbarComponent },
+
   { path: '**', redirectTo: ' ' },
 ];
